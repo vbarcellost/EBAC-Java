e2574d0e201d12c0c209656da84559d683eee865
Vitória Barcellos <vbarcellost@gmail.com>
2026-05-12 00:24:42 -0300

    Create ObjetoTabela.java

---
+++ ObjetoTabela.java
@@ -0,0 +1,12 @@
+public class ObjetoTabela {
+
+    private String nomeTabela;
+
+    public ObjetoTabela(String nomeTabela) {
+        this.nomeTabela = nomeTabela;
+    }
+
+    public String getNomeTabela() {
+        return nomeTabela;
+    }
+}
